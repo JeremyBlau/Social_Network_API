f9cf2f82a012870107e81cef3799c279d05f4ee8
jwblau2 <jeremy@abundantly.com>
2023-10-26 20:01:51 -0600

    added to mongoos.js & server.js

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-ts
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Import routes
+const userRoutes = require('./routes/api/users');
+const thoughtRoutes = require('./routes/api/thoughts');
+const reactionRoutes = require('./routes/api/reactions');
+
+// Use routes
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+app.use('/api/thoughts/:thoughtId/reactions', reactionRoutes);
+
+// Default response for unmatched routes
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
+
+// Mongoose connection
+mongoose.connect('mongodb://localhost/social-media', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
